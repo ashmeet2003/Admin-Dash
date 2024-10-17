6db3823f048e890fb9bac634cd3e622bb7b5312f
Ashmeet Singh <ashmeetsingh812@gmail.com>
2024-10-17 12:54:47 +0530

    first commit

---
+++ server/controllers/client.js
@@ -0,0 +1,109 @@
+import Product from "../models/Product.js";
+import ProductStat from "../models/ProductStat.js";
+import User from "../models/User.js";
+import Transaction from "../models/Transaction.js";
+import getCountryIso3 from "country-iso-2-to-3";
+
+export const getProducts = async (req, res) => {
+  try {
+    //getting products
+    const products = await Product.find();
+
+    //using product id to get their stat -- slow query 
+    const productsWithStats = await Promise.all(
+      products.map(async (product) => {
+        const stat = await ProductStat.find({
+          productId: product._id,
+        });
+      //console.log(stat);
+        return {
+          ...product._doc,
+          stat,
+        };
+      })
+    );
+
+    res.status(200).json(productsWithStats);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getCustomers = async (req, res) => {
+  try {
+    const customers = await User.find({ role: "user" }).select("-password");
+    res.status(200).json(customers);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getTransactions = async (req, res) => {
+  try {
+    // sort (should look like this) : { "field": "userId", "sort": "desc"} from frontend MUI as a string
+    const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;
+
+    // formatted sort should look like { userId: -1 } for mongodb
+    // below function to format it
+    const generateSort = () => {
+      const sortParsed = JSON.parse(sort);  //string to JSON obj
+      const sortFormatted = {
+        [sortParsed.field]: (sortParsed.sort = "asc" ? 1 : -1),
+      };
+
+      return sortFormatted;
+    };
+    const sortFormatted = Boolean(sort) ? generateSort() : {};
+
+    const transactions = await Transaction.find({
+      $or: [
+        { cost: { $regex: new RegExp(search, "i") } },  //allow us to search cost field
+        { userId: { $regex: new RegExp(search, "i") } },  //search usedId
+      ],
+    })
+      .sort(sortFormatted)
+      .skip(page * pageSize)
+      .limit(pageSize);
+
+    //counting total amount of query
+    const total = await Transaction.countDocuments({
+      $or: [
+        { cost: { $regex: new RegExp(search, "i") } },
+        { userId: { $regex: new RegExp(search, "i") } },
+      ],
+    });
+    
+    console.log(total);
+    res.status(200).json({
+      transactions,
+      total,
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getGeography = async (req, res) => {
+  try {
+    const users = await User.find();
+    //formatting acc to nivo charts, creating obj below
+    const mappedLocations = users.reduce((acc, { country }) => {
+      const countryISO3 = getCountryIso3(country); //from package
+      if (!acc[countryISO3]) {
+        acc[countryISO3] = 0;
+      }
+      acc[countryISO3]++;
+      return acc;
+    }, {});
+    //using object above to convert to proper format
+    const formattedLocations = Object.entries(mappedLocations).map(
+      ([country, count]) => {
+        return { id: country, value: count };
+      }
+    );
+
+    res.status(200).json(formattedLocations);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
